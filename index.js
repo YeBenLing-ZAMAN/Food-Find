e41bc5c8c80e66faeef4342967908d8dfe201314
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-02-25 19:50:35 +0600

    api call and show items in html

---
+++ index.js
@@ -0,0 +1,43 @@
+
+
+function search() {
+    const searchInputField = document.getElementById('search-input-field').value;
+
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInputField}`)
+        .then(res => res.json())
+        .then(data => showResult(data.meals))
+}
+
+showResult = (data) => {
+    const addResultDiv = document.getElementById('add-search-result');
+
+    const foundSearchResult = document.getElementById('search-result');
+
+    //  search reulte show
+    foundSearchResult.innerText = ``;
+    if(data.length>0){
+        foundSearchResult.innerText =   `${data.length} result is found`;
+    }else{
+        foundSearchResult.innerText =   `No result is found`;
+    }
+
+    console.log(data);
+    for (const items of data) {
+        const div = document.createElement('div');
+        div.innerHTML = `
+                        <div class="card text-center">
+                            <img src="${items.strMealThumb}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">${items.strMeal}</h5>
+                                <p class="card-text">${items.strInstructions.substr(0,100)}</p>
+                                <a href="#" class="btn btn-warning"> more details</a>
+                            </div>
+                        </div>
+    `
+    addResultDiv.appendChild(div);
+    }
+    const searchInputField = document.getElementById('search-input-field').value='';
+
+}
+
+const searchResultDiv = document.getElementById('add-search-result');
