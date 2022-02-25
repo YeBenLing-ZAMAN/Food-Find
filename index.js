861f0b816bf239dcfc8753b7509e31fa9797471b
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-02-25 20:02:24 +0600

    error handling added

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 
 
-function search() {
+const search = () => {
     const searchInputField = document.getElementById('search-input-field').value;
 
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInputField}`)
+    const url =`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInputField}`;
+    if(searchInputField == ''){
+
+    }else{
+
+        fetch(url)
         .then(res => res.json())
         .then(data => showResult(data.meals))
+    }
 }
+
 
 showResult = (data) => {
     const addResultDiv = document.getElementById('add-search-result');
@@ -14,29 +21,39 @@ showResult = (data) => {
     const foundSearchResult = document.getElementById('search-result');
 
     //  search reulte show
+    // Before show it clear previous items 
     foundSearchResult.innerText = ``;
+    // addResultDiv.innerHTML=''; // use kora thik na problem : memory lick 
+    addResultDiv.textContent=''; // age ta theke better 
+
+
     if(data.length>0){
+
         foundSearchResult.innerText =   `${data.length} result is found`;
+        
+        // show result as a block items 
+        for (const items of data) {
+            const div = document.createElement('div');
+            div.innerHTML = `
+                            <div class="card text-center">
+                                <img src="${items.strMealThumb}" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">${items.strMeal}</h5>
+                                    <p class="card-text">${items.strInstructions.substr(0,100)}</p>
+                                    <a href="#" class="btn btn-warning"> more details</a>
+                                </div>
+                            </div>
+        `
+        addResultDiv.appendChild(div);
+        }
+        const searchInputField = document.getElementById('search-input-field').value='';
     }else{
         foundSearchResult.innerText =   `No result is found`;
     }
 
     console.log(data);
-    for (const items of data) {
-        const div = document.createElement('div');
-        div.innerHTML = `
-                        <div class="card text-center">
-                            <img src="${items.strMealThumb}" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">${items.strMeal}</h5>
-                                <p class="card-text">${items.strInstructions.substr(0,100)}</p>
-                                <a href="#" class="btn btn-warning"> more details</a>
-                            </div>
-                        </div>
-    `
-    addResultDiv.appendChild(div);
-    }
-    const searchInputField = document.getElementById('search-input-field').value='';
+
+    
 
 }
 
